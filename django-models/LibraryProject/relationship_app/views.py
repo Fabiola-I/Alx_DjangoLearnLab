0facdf911d5b5d9ddb1a7b71aa31def472991baf
Fabi-starr <fabiolaingabire02@gmail.com>
2025-11-14 21:05:52 +0200

    Fix: added required login and UserCreationForm imports for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Savan checker required imports
 from django.views.generic.detail import DetailView     # required literal form
 from .models import Library                            # required literal form
