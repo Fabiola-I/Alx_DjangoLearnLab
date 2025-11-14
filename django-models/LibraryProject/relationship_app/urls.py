6c08dd400b441da7fe19245aeba10c1bc41cbb0c
Fabi-starr <fabiolaingabire02@gmail.com>
2025-11-14 20:51:56 +0200

    Fix: added required import for list_books in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,8 @@ from .librarian_view import librarian_view
 from .member_view import member_view
 from .views import LibraryDetailView
 from . import auth_views
+from django.urls import path
+from .views import list_books 
 app_name = 'relationship_app'
 
 urlpatterns = [
@@ -13,6 +15,7 @@ urlpatterns = [
 
     # role-based views
     path('role/admin/', admin_view, name='admin_view'),
+    path("books/", list_books, name="list_books"),
     path('role/librarian/', librarian_view, name='librarian_view'),
     path('role/member/', member_view, name='member_view'),
 
