5224d82e0aca5af3c0b85e0d6189e764f79bc80d
Fabi-starr <fabiolaingabire02@gmail.com>
2025-12-14 22:01:12 +0200

    Initial commit: setup Django project with apps and URLs

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.authtoken.models import Token
+
+from .serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
+from .models import User
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ProfileView(APIView):
+    def get(self, request):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
