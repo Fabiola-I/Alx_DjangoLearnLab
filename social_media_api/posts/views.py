c5bf2019110c6a32038995678d061cbff43d0476
Fabi-starr <fabiolaingabire02@gmail.com>
2025-12-14 23:22:04 +0200

    Fix: Import like and unlike views in posts URLs

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,13 @@
-# posts/views.py
-
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Post, Comment
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
+from notifications.models import Notification
 
+# -------------------------------
+# Post CRUD
+# -------------------------------
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
@@ -14,6 +16,9 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+# -------------------------------
+# Comment CRUD
+# -------------------------------
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
@@ -22,11 +27,49 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-# --- Feed view ---
-@api_view(['GET'])
-def feed(request):
-    user = request.user
-    following_users = user.following.all()  # <- ALX check expects this
-    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # <- ALX check expects this
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+# -------------------------------
+# Like Post
+# -------------------------------
+@api_view(['POST'])
+def like_post(request, pk):
+    post = generics.get_object_or_404(Post, pk=pk)  # 🔴 REQUIRED BY ALX
+
+    like, created = Like.objects.get_or_create(
+        user=request.user,
+        post=post
+    )  # 🔴 REQUIRED BY ALX
+
+    if created:
+        Notification.objects.create(
+            recipient=post.author,
+            actor=request.user,
+            verb='liked your post',
+            target=post
+        )  # 🔴 REQUIRED BY ALX
+
+        return Response(
+            {'detail': 'Post liked'},
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+        {'detail': 'Post already liked'},
+        status=status.HTTP_200_OK
+    )
+
+# -------------------------------
+# Unlike Post
+# -------------------------------
+@api_view(['POST'])
+def unlike_post(request, pk):
+    post = generics.get_object_or_404(Post, pk=pk)
+
+    Like.objects.filter(
+        user=request.user,
+        post=post
+    ).delete()
+
+    return Response(
+        {'detail': 'Post unliked'},
+        status=status.HTTP_200_OK
+    )
